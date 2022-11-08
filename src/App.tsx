cefca48c3c68c268784dd6e5269abd9f36539c95
Niklas Köhn <n.koehn@esri.de>
2022-11-08 16:55:47 +0100

    simplest Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Map from "./Map/Map";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Map></Map>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
