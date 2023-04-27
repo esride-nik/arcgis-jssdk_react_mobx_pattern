7e0b38f3b2be376dd2ab52efbc51d86c92d6fa2b
Niklas Köhn <n.koehn@esri.de>
2023-04-27 13:39:07 +0200

    demo button

--- src/Status/StatusCmp.tsx
+++ src/Status/StatusCmp.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { observer } from "mobx-react";
 import { useEffect } from "react";
 import { useStores } from "../Stores/useStores";
+import { useMapContext } from "../Map/useMapContext";
 import "./Status.css";
 
 interface StatusCmpProps {}
@@ -9,6 +10,11 @@ interface StatusCmpProps {}
 const StatusCmp: React.FC<StatusCmpProps> = observer(
   (props: StatusCmpProps) => {
     const { statusStore, mapStore } = useStores();
+    const mapContext = useMapContext();
+
+    const goSomewhere = () => {
+      mapContext.mapView.goTo({ target: [0,0], zoom: 5 }, {animate: true, duration: 3000})
+    }
 
     useEffect(() => {
       statusStore.setChangeCounter(statusStore.changeCounter + 1);
@@ -19,6 +25,7 @@ const StatusCmp: React.FC<StatusCmpProps> = observer(
       <div id="status">
         <span>{statusStore.statusMessage}</span>
         <span>{mapStore.center.x} / {mapStore.center.y}</span>
+        <button onClick={goSomewhere}>Somewhere in the middle</button>
       </div>
     );
   }
