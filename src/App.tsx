e0bd5b08dad804a0170567f550a9a7cb56008dac
Niklas Köhn <n.koehn@esri.de>
2023-04-27 11:51:47 +0200

    huh two maps?

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
       <MapProvider>
-        <MapCmp></MapCmp>
+        <MapCmp/>
       </MapProvider>
-      <StatusCmp></StatusCmp>
+      <StatusCmp/>
     </div>
   );
 }
--- src/Status/StatusCmp.tsx
+++ src/Status/StatusCmp.tsx
@@ -3,12 +3,19 @@ import { observer } from "mobx-react";
 import { useEffect } from "react";
 import { useStores } from "../Stores/useStores";
 import "./Status.css";
+import { useMapContext } from "../Map/useMapContext";
 
 interface StatusCmpProps {}
 
 const StatusCmp: React.FC<StatusCmpProps> = observer(
   (props: StatusCmpProps) => {
     const { statusStore, mapStore } = useStores();
+    // const mapContext = useMapContext();
+    
+    const goSomewhere = () => {
+      // mapContext.mapView.goTo([0,0]);
+      console.log('lalala');
+    }
 
     useEffect(() => {
       statusStore.setChangeCounter(statusStore.changeCounter + 1);
@@ -19,6 +26,7 @@ const StatusCmp: React.FC<StatusCmpProps> = observer(
       <div id="status">
         <span>{statusStore.statusMessage}</span>
         <span>{mapStore.center.x} / {mapStore.center.y}</span>
+        <button onClick={goSomewhere}>Somewhere in the middle</button>
       </div>
     );
   }
