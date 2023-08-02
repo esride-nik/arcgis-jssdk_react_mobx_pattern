50f4229996d61bc6878b24e3b70e164872cf4d19
Niklas Köhn <n.koehn@esri.de>
2023-08-02 08:10:05 +0200

    MapCmp: observer fix

--- src/Map/MapCmp.tsx
+++ src/Map/MapCmp.tsx
@@ -1,21 +1,30 @@
-// ===================================================================
-// For the purpose of the exercise, don't change anything in this file
-// ===================================================================
-
 import * as React from "react";
 import { useEffect } from "react";
 import { useMapContext } from "./useMapContext";
 import "./Map.css";
+import { useStores } from "Stores/useStores";
+import { observer } from "mobx-react";
 
 interface MapCmpProps {}
 
-const MapCmp: React.FC<MapCmpProps> = (props: MapCmpProps) => {
+const MapCmp: React.FC<MapCmpProps> = observer((props: MapCmpProps) => {
   const mapContext = useMapContext();
+  const { mapStore, sceneStore } = useStores();
 
   // map must be initialized after first render, because we need the DOM node ref
   useEffect(mapContext.initMap, []);
 
-  return <div ref={mapContext.mapNode} id="map" />;
-};
+  useEffect(() => {
+    mapContext.mapView.center = sceneStore.center;
+  }, [sceneStore.center])
+  
+  useEffect(() => {
+    console.log("map center", mapStore.center);
+  }, [mapStore.center]);
+
+  return <>
+    <div ref={mapContext.mapNode} id="map" />
+  </>;
+});
 
 export default MapCmp;
--- src/Status/StatusCmp.tsx
+++ src/Status/StatusCmp.tsx
@@ -4,16 +4,19 @@ import { useEffect } from "react";
 import { useStores } from "../Stores/useStores";
 import { useMapContext } from "../Map/useMapContext";
 import "./Status.css";
+import { useSceneContext } from "../Scene/useSceneContext";
 
 interface StatusCmpProps {}
 
 const StatusCmp: React.FC<StatusCmpProps> = observer(
   (props: StatusCmpProps) => {
-    const { statusStore, mapStore } = useStores();
+    const { statusStore, mapStore, sceneStore } = useStores();
     const mapContext = useMapContext();
+    const sceneContext = useSceneContext();
 
     const goSomewhere = () => {
       mapContext.mapView.goTo({ target: [0,0], zoom: 5 }, {animate: true, duration: 3000})
+      sceneContext.sceneView.goTo({ target: [0,0], zoom: 5 }, {animate: true, duration: 3000})
     }
 
     useEffect(() => {
@@ -24,7 +27,8 @@ const StatusCmp: React.FC<StatusCmpProps> = observer(
     return (
       <div id="status">
         <span>{statusStore.statusMessage}</span>
-        <span>{mapStore.center.x} / {mapStore.center.y}</span>
+        <span>map {mapStore.center.x} / {mapStore.center.y}</span>
+        <span>scene {sceneStore.center.x} / {sceneStore.center.y}</span>
         <button onClick={goSomewhere}>Somewhere in the middle</button>
       </div>
     );
