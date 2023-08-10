ba6247806ff69ef14d05e1718c1beb2b46c34c93
Niklas Köhn <n.koehn@esri.de>
2023-08-10 15:12:04 +0200

    URL construct issue: setAssetPath

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,22 @@ import { MapProvider } from "./Map/MapProvider";
 import StatusCmp from "./Status/StatusCmp";
 import { SceneProvider } from "Scene/SceneProvider";
 
+import "@arcgis/core/assets/esri/themes/light/main.css";
 import '@esri/calcite-components/dist/components/calcite-shell';
 import '@esri/calcite-components/dist/components/calcite-panel';
 import '@esri/calcite-components/dist/components/calcite-shell-panel';
 import { CalciteShell, CalcitePanel, CalciteShellPanel } from '@esri/calcite-components-react';
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+
+setAssetPath("https://unpkg.com/@esri/calcite-components/dist/calcite/assets");
 
 /*
+resizable feature:
+
 Adam Tirella 18:28
 Niklas Köhn (Guest) - Shell Panel css width: https://developers.arcgis.com/calcite-design-system/components/shell-panel/#styles
 codepen: https://codepen.io/mac_and_cheese/pen/NWEVwEa?editors=1000
+
 
  */
 
